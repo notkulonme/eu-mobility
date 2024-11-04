936bd8532b85373085b1bfed22191d6811699287
notkulunme <nricsi006@gmail.com>
2024-11-04 12:38:27 +0100

    better print

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter your username:");
+        System.out.print("Enter your username: ");
         final var scanner = new Scanner(System.in);
         final var userName = scanner.nextLine();
         System.out.printf("Hello, %s!%n", userName);
